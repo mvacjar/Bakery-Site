64d2148d1e6c90c0b8f961a451f85475ff6a5696
mvacjar <mvacjar@gmail.com>
2024-04-05 20:18:57 +0200

    Footer Done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
+
+import { useEffect } from "react";
+import { Link } from "react-scroll";
+import { FaCopyright } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 500,
+      easing: "ease-in-sine",
+    });
+  }, []);
+
+  return (
+    <>
+      <section className="bg-black  text-white flex justify-center items-center gap-1">
+        <FaCopyright className="size-4 text-white" />
+        <h1 className="text-white text-center font-semibold">
+          Bakery Delight, All Rights Reserve
+        </h1>
+      </section>
+      <div
+        className="bg-orange-400 text-black p-3 rounded-full hover:bg-white cursor-pointer fixed bottom-5 right-2 "
+        id="icon-box"
+      >
+        <Link to="hero" spy={true} offset={-100} smooth={true}>
+          <FaAngleUp className="w-[20px] h-[20px]" />{" "}
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
